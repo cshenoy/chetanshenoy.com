11da4278f0de234818a20447b25a4cf8de824c9b
Chetan Shenoy <me@chetanshenoy.com>
2011-04-10 23:28:13 -0400

    first commit

---
+++ public/docs/js/parallax.js
@@ -0,0 +1,72 @@
+/*
+Author: Chetan Shenoy
+Author URI: http://www.chetanshenoy.com/
+
+Huge thanks to Ian Lunn [http://www.ianlunn.co.uk/]
+
+License: http://creativecommons.org/licenses/by-sa/3.0/ (Attribution Share Alike). Please attribute work to Ian Lunn simply by leaving these comments in the source code or if you'd prefer, place a link on your website to http://www.ianlunn.co.uk/.
+
+Dual licensed under the MIT and GPL licenses:
+http://www.opensource.org/licenses/mit-license.php
+http://www.gnu.org/licenses/gpl.html
+*/
+
+$(document).ready(function() {
+
+	var $window = $(window);
+	var $bckg = $('img#bg');
+	var $firstBG = $('body');
+
+	var windowHeight = $window.height();
+	
+	//apply the class "inview" to a section that is in the viewport
+	$('#intro, #second, #third, #fourth').bind('inview', function (event, visible) {
+			if (visible == true) {
+			$(this).addClass("inview");
+			} else {
+			$(this).removeClass("inview");
+			}
+		});
+			
+	//function that places the navigation in the center of the window
+	function RepositionNav(){
+		var windowHeight = $window.height(); //get the height of the window
+		var navHeight = $('#nav').height() / 2;
+		var windowCenter = (windowHeight / 2); 
+		var newtop = windowCenter - navHeight;
+		$('#nav').css({"top": newtop}); //set the new top position of the navigation list
+	}
+	
+	//function that is called for every pixel the user scrolls. Determines the position of the background
+	/*arguments: 
+		x = horizontal position of background
+		windowHeight = height of the viewport
+		pos = position of the scrollbar
+		adjuster = adjust the position of the background
+		inertia = how fast the background moves in relation to scrolling; lower # = moves slow
+	*/
+	function newPos(x, windowHeight, pos, adjuster, inertia){
+		return /*x + "% " +*/ (-((windowHeight + pos) - adjuster) * inertia)  + "px";
+	}
+	
+	//function to be called whenever the window is scrolled or resized
+	function Move(){ 
+		var pos = $window.scrollTop(); //position of the scrollbar
+
+		$bckg.css({'top': newPos(0, windowHeight, pos, 400, 0.2)}); 
+				
+		$('#pixels').html(pos); //display the number of pixels scrolled at the bottom of the page
+	}
+		
+	RepositionNav(); //Reposition the Navigation to center it in the window when the script loads
+	
+	$window.resize(function(){ //if the user resizes the window...
+		Move(); //move the background images in relation to the movement of the scrollbar
+		RepositionNav(); //reposition the navigation list so it remains vertically central
+	});		
+	
+	$window.bind('scroll', function(){ //when the user is scrolling...
+		Move(); //move the background images in relation to the movement of the scrollbar
+	});
+	
+});
